867e58346b17c2df43cb1fc6785c3d3d51d79520
fshrmnsfrnd <145749103+fshrmnsfrnd@users.noreply.github.com>
2024-10-03 22:38:49 +0200

    Works
    First 3 Shortcuts working in chrome

--- background.js
+++ background.js
@@ -1,20 +1,64 @@
-console.log("Background . js läuft");
-
-function UnSetBold() {
-  document.getElementById('textToolbarBoldStyle').click();
-}
+console.log("Background.js läuft");
 
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "UnSetBold") {
-      console.log("Alt + B successful.");
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: UnSetBold()
-        });
-      });
-    }
-  });
-  
+  console.log("Command successful.");
 
-  
+  if (command === "UnSetBold") {
+    console.log("Alt + B erfolgreich erkannt.");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: UnSetBold
+      });
+    });
+  }else if (command === "UnSetItalic") {
+    console.log("Alt + I erfolgreich erkannt.");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: UnSetItalic
+      });
+    });
+  }else if (command === "UnSetUnderline") {
+    console.log("Alt + U erfolgreich erkannt.");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: UnSetUnderline
+      });
+    });
+  }
+});
+
+function UnSetBold() {
+  const buttonBold = document.getElementById('textToolbarBoldStyle');
+  if (buttonBold) {
+    buttonBold.click();
+    console.log("Bold-Toggle ausgeführt.");
+  } else {
+    console.log("Button nicht gefunden.");
+  }
+}
+
+function UnSetItalic() {
+  const buttonItalic = document.getElementById('textToolbarItalicStyle');
+  if (buttonItalic) {
+    buttonItalic.click();
+    console.log("Italic-Toggle ausgeführt.");
+  } else {
+    console.log("Italic nicht gefunden.");
+  }
+}
+
+function UnSetUnderline() {
+  const buttonUnderline = document.getElementById('textToolbarUnderlineStyle');
+  if (buttonUnderline) {
+    buttonUnderline.click();
+    console.log("Underline-Toggle ausgeführt.");
+  } else {
+    console.log("Underline nicht gefunden.");
+  }
+}
---
+++ contentScript.js
@@ -0,0 +1,9 @@
+/*function UnSetBold() {
+  const button = document.getElementById('textToolbarBoldStyle');
+  if (button) {
+    button.click();
+    console.log("Bold-Toggle im Content Script ausgeführt.");
+  } else {
+    console.log("Button nicht gefunden.");
+  }
+}*/
